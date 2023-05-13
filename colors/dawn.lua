425a0cb167773db5ddfdce97e6703a7b35b3eb62
AlphaKeks <alphakeks@dawn.sh>
2023-05-13 21:47:54 +0200

    feat: change structure
    - remove `Dawn` global
    - add `setup` function
    - add `palette`

--- colors/dawn.lua
+++ colors/dawn.lua
@@ -17,42 +17,7 @@ let g:colors_name="dawn"
 
 ]])
 
-Dawn = {
-  rosewater = "#F5E0DC",
-  flamingo  = "#F2CDCD",
-  pink      = "#F5C2E7",
-  mauve     = "#CBA6F7",
-  red       = "#F38BA8",
-  maroon    = "#EBA0AC",
-  peach     = "#FAB387",
-  yellow    = "#F9E2AF",
-  green     = "#A6E3A1",
-  teal      = "#94E2D5",
-  sky       = "#89DCEB",
-  sapphire  = "#74C7EC",
-  blue      = "#89B4FA",
-  lavender  = "#B4BEFE",
-
-  text      = "#CDD6F4",
-  subtext1  = "#BAC2DE",
-  subtext0  = "#A6ADC8",
-  overlay2  = "#9399B2",
-  overlay1  = "#7F849C",
-  overlay0  = "#6C7086",
-  surface2  = "#585B70",
-  surface1  = "#45475A",
-  surface0  = "#313244",
-
-  base      = "#1E1E2E",
-  mantle    = "#181825",
-  crust     = "#11111B",
-
-  none      = "NONE",
-  slate     = "#3C5E7F",
-  poggers   = "#7480C2",
-
-  transparent = true,
-}
+local Dawn = require("dawn").palette
 
 local function hi(group, opts)
   if not opts then return end
@@ -65,7 +30,9 @@ hi("ColorColumn", {
   bg = Dawn.surface0,
 })
 
-hi("Conceal")
+hi("Conceal", {
+  bg = Dawn.none,
+})
 
 hi("Cursor", {
   fg = Dawn.base,
@@ -272,7 +239,10 @@ hi("WildMenu", {
   bg = Dawn.surface2,
 })
 
-hi("Winbar")
+hi("Winbar", {
+  fg = Dawn.pink,
+  italic = true,
+})
 
 hi("DiagnosticOk", {
   fg = Dawn.green,
